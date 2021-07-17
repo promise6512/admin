b9ca226ff3336ece485e3df3fbfd3e23036f4684
hjp <2523863783@qq.com>
2021-07-17 16:38:38 +0800

    antd config

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
-
+import './App.less'
+import {Button} from 'antd'
 export default class App extends Component {
   render() {
     return (
       <div>
         APP..
+        <Button type="primary">Button</Button>
       </div>
     )
   }
