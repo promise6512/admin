ce107fc398850ee3bd302aaff509ae98e6b8f52a
hjp <2523863783@qq.com>
2021-07-19 11:22:55 +0800

    episode37 static header has done

--- src/pages/admin/admin.jsx
+++ src/pages/admin/admin.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Redirect,Switch,Route } from 'react-router-dom'
+import { Redirect, Switch, Route } from 'react-router-dom'
 import memoryUtils from '../../utils/memoryUtils'
 import Header from '../../components/header/header';
 import LeftNav from '../../components/left-nav/left-nav';
@@ -13,7 +13,7 @@ import Bar from '../charts/bar';
 import Line from '../charts/line';
 import Pie from '../charts/pie'
 import { Layout } from 'antd';
-const {  Footer, Sider, Content } = Layout;
+const { Footer, Sider, Content } = Layout;
 export default class Admin extends Component {
     render() {
         const { user } = memoryUtils
@@ -23,26 +23,33 @@ export default class Admin extends Component {
             )
         }
         return (
-            <Layout style={{height:'100%'}}>
+            <Layout style={{ height: '100%' }}>
                 <Sider>
-                    <LeftNav/>
+                    <LeftNav />
                 </Sider>
                 <Layout>
                     <Header>Header</Header>
-                    <Content style={{background:'#fff'}}>
+                    <Content style={{ background: '#fff',margin:'20px' }}>
                         <Switch>
-                            <Route path='/home' component={Home}/>
-                            <Route path='/category' component={Category}/>
-                            <Route path='/product' component={Product}/>
-                            <Route path='/role' component={Role}/>
-                            <Route path='/user' component={User}/>
-                            <Route path='/charts/bar' component={Bar}/>
-                            <Route path='/charts/line' component={Line}/>
-                            <Route path='/charts/pie' component={Pie}/>
-                            <Redirect to='/home' />
+                            <Route path='/home' component={Home} />
+                            <Route path='/category' component={Category} />
+                            <Route path='/product' component={Product} />
+                            <Route path='/role' component={Role} />
+                            <Route path='/user' component={User} />
+                            <Route path='/charts/bar' component={Bar} />
+                            <Route path='/charts/line' component={Line} />
+                            <Route path='/charts/pie' component={Pie} />
+                            <Redirect to='/home' /> 
+                           {/*  <Redirect
+                                to={{
+                                    pathname: "/home",
+                                    state: { referrer: '/home'}
+                                }}
+                            /> */}
+
                         </Switch>
                     </Content>
-                    <Footer style={{textAlign:'center',color:'#AAA'}}>
+                    <Footer style={{ textAlign: 'center', color: '#AAA' }}>
                         推荐使用谷歌浏览器,可以获得最佳页面操作体验
                     </Footer>
                 </Layout>
